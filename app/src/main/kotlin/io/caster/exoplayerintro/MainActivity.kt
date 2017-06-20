d30a116712ba29a5fa1d22b710d6cfe871fd9cf5
AndrewOrobator <Orobator.Andrew@gmail.com>
2017-06-20 19:47:30 -0400

    Removed unused import Updated gradle to 4.0 Updated kotlin and android gradle plugin

--- app/src/main/kotlin/io/caster/exoplayerintro/MainActivity.kt
+++ app/src/main/kotlin/io/caster/exoplayerintro/MainActivity.kt
@@ -8,7 +8,6 @@ import com.google.android.exoplayer2.ExoPlayer
 import com.google.android.exoplayer2.ExoPlayerFactory
 import com.google.android.exoplayer2.extractor.DefaultExtractorsFactory
 import com.google.android.exoplayer2.source.ExtractorMediaSource
-import com.google.android.exoplayer2.source.MediaSource
 import com.google.android.exoplayer2.trackselection.DefaultTrackSelector
 import com.google.android.exoplayer2.upstream.DefaultDataSourceFactory
 import com.google.android.exoplayer2.util.Util
